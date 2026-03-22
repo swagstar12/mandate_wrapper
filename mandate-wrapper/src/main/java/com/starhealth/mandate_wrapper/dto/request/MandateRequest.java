fd99d764fadaf89934478525cfe5de9eba5f8971
Pranav Anil Bhange <130270817+swagstar12@users.noreply.github.com>
2026-03-22 18:55:54 +0530

    Update MandateRequest.java

--- mandate-wrapper/src/main/java/com/starhealth/mandate_wrapper/dto/request/MandateRequest.java
+++ mandate-wrapper/src/main/java/com/starhealth/mandate_wrapper/dto/request/MandateRequest.java
@@ -1,12 +1,31 @@
 package com.starhealth.mandate_wrapper.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 @Data
 public class MandateRequest{
+    @JsonProperty("order_id")
+    @JsonAlias({"order_id", "orderId"})
     private String orderId;
+
     private String amount;
-    private String customer_id;
-    private String mandate_id;
-    private String execution_date;
-    private String merchant_id;
+
+    @JsonProperty("customer_id")
+    @JsonAlias({"customer_id", "customerId"})
+    private String customerId;
+
+    @JsonProperty("mandate_id")
+    @JsonAlias({"mandate_id", "mandateId"})
+    private String mandateId;
+
+    @JsonProperty("execution_date")
+    @JsonAlias({"execution_date", "executionDate"})
+    private String executionDate;
+
+    @JsonProperty("merchant_id")
+    @JsonAlias({"merchant_id", "merchantId"})
+    private String merchantId;
+
     private String format;
 }
